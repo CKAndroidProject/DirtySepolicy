4e7c62f2206d90503f9a7aade8187eccda3b5a52
vvb2060 <vvb2060@gmail.com>
2026-05-29 03:40:23 +0800

    rewrite checkSELinuxAccess

--- app/src/main/java/org/lsposed/dirtysepolicy/AppZygote.java
+++ app/src/main/java/org/lsposed/dirtysepolicy/AppZygote.java
@@ -3,20 +3,12 @@ package org.lsposed.dirtysepolicy;
 import android.app.ZygotePreload;
 import android.content.pm.ApplicationInfo;
 import android.os.Build;
-import android.os.SELinux;
 import android.system.ErrnoException;
 import android.system.Os;
-import android.system.OsConstants;
 import android.util.Log;
 
 import java.io.File;
 import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 
 public final class AppZygote implements ZygotePreload {
@@ -39,9 +31,6 @@ public final class AppZygote implements ZygotePreload {
         if (!context.equals(procContext)) {
             return "ERROR: /proc/self context mismatch: " + procContext;
         }
-        if (!SELinux.isSELinuxEnforced()) {
-            return "ERROR: SELinux is permissive";
-        }
         if (!SELinux.checkSELinuxAccess("u:r:app_zygote:s0", "u:r:app_zygote:s0", "process", "setcurrent")) {
             return "ERROR: cannot check SELinux access";
         }
@@ -49,33 +38,36 @@ public final class AppZygote implements ZygotePreload {
             return "ERROR: cannot check SELinux context";
         }
         var sb = new StringBuilder();
+        if (!SELinux.isSELinuxEnforced()) {
+            sb.append("SELinux is permissive; ");
+        }
         if (SELinux.checkSELinuxAccess("u:r:system_server:s0", "u:r:system_server:s0", "process", "execmem")) {
             sb.append("system_server can execmem; ");
         }
         if (Build.TYPE.equals("user") && SELinux.checkSELinuxAccess("u:r:shell:s0", "u:r:su:s0", "process", "transition")) {
             sb.append("found AOSP su in user build; ");
         }
-        if (contextExists("u:r:adbroot:s0")
+        if (SELinux.contextExists("u:r:adbroot:s0")
                 || SELinux.checkSELinuxAccess("u:r:adbd:s0", "u:r:adbroot:s0", "binder", "call")) {
             sb.append("found adb_root; ");
         }
-        if (contextExists("u:r:magisk:s0") || contextExists("u:object_r:magisk_file:s0")
+        if (SELinux.contextExists("u:r:magisk:s0") || SELinux.contextExists("u:object_r:magisk_file:s0")
                 || SELinux.checkSELinuxAccess("u:r:untrusted_app:s0", "u:object_r:magisk_file:s0", "file", "read")
                 || SELinux.checkSELinuxAccess("u:object_r:rootfs:s0", "u:object_r:tmpfs:s0", "filesystem", "associate")
                 || SELinux.checkSELinuxAccess("u:r:kernel:s0", "u:object_r:tmpfs:s0", "fifo_file", "open")) {
             sb.append("found Magisk; ");
         }
-        if (contextExists("u:r:ksu:s0") || contextExists("u:object_r:ksu_file:s0")
+        if (SELinux.contextExists("u:r:ksu:s0") || SELinux.contextExists("u:object_r:ksu_file:s0")
                 || SELinux.checkSELinuxAccess("u:r:kernel:s0", "u:object_r:adb_data_file:s0", "file", "read")
                 || SELinux.checkSELinuxAccess("u:r:untrusted_app:s0", "u:object_r:ksu_file:s0", "file", "read")) {
             sb.append("found KernelSU; ");
         }
-        if (contextExists("u:object_r:lsposed_file:s0")
+        if (SELinux.contextExists("u:object_r:lsposed_file:s0")
                 || SELinux.checkSELinuxAccess("u:r:untrusted_app:s0", "u:object_r:lsposed_file:s0", "file", "read")
                 || SELinux.checkSELinuxAccess("u:r:system_server:s0", "u:object_r:apk_data_file:s0", "file", "execute")) {
             sb.append("found LSPosed; ");
         }
-        if (contextExists("u:object_r:xposed_data:s0") || contextExists("u:object_r:xposed_file:s0")
+        if (SELinux.contextExists("u:object_r:xposed_data:s0") || SELinux.contextExists("u:object_r:xposed_file:s0")
                 || SELinux.checkSELinuxAccess("u:r:untrusted_app:s0", "u:object_r:xposed_data:s0", "file", "read")
                 || SELinux.checkSELinuxAccess("u:r:dex2oat:s0", "u:object_r:dex2oat_exec:s0", "file", "execute_no_trans")) {
             sb.append("found Xposed; ");
@@ -84,7 +76,7 @@ public final class AppZygote implements ZygotePreload {
             sb.append("found ZygiskNext; ");
         }
 
-        var buffer = readStatus();
+        var buffer = SELinux.readStatus();
         int version = buffer.getInt(0);
         if (version != 1) {
             return "ERROR: unknown status version: " + version;
@@ -92,21 +84,28 @@ public final class AppZygote implements ZygotePreload {
         int sequence = buffer.getInt(4);
         int enforcing = buffer.getInt(8);
         if (enforcing != 1) {
-            sb.append("SELinux is permissive; ");
+            sb.append("enforcing=").append(enforcing).append("; ");
         }
         int policyload = buffer.getInt(12);
         int deny_unknown = buffer.getInt(16);
         if (deny_unknown != 1) {
             sb.append("deny_unknown=").append(deny_unknown).append("; ");
         }
-        var avdSeqNo = readAvdSeqNo();
-        if ((policyload == 0 && sequence != 0) || (policyload == 1 && sequence != 4) || policyload > 1 || avdSeqNo != 1) {
-            sb.append("sequence=").append(sequence).append(" policyload=").append(policyload);
-            sb.append(" avdSeqNo=").append(avdSeqNo).append("; ");
+        if ((policyload == 0 && sequence != 0) || (policyload == 1 && sequence != 4) || policyload > 1) {
+            sb.append("sequence=").append(sequence).append(" policyload=").append(policyload).append("; ");
+        }
+        try {
+            var avd = SELinux.access("u:r:untrusted_app:s0", "u:r:untrusted_app:s0", 0);
+            var avdSeqNo = Integer.parseInt(avd[4]);
+            if (avdSeqNo != 1) {
+                sb.append("avdSeqNo=").append(avdSeqNo).append("; ");
+            }
+        } catch (ErrnoException e) {
+            throw new RuntimeException(e);
         }
         if (sb.length() == 0) {
             return "OK: no dirty sepolicy found\n" +
-                    "INFO: sequence=" + sequence + " policyload=" + policyload + " avdSeqNo=" + avdSeqNo;
+                    "INFO: sequence=" + sequence + " policyload=" + policyload;
         } else {
             return "WARNING: " + sb;
         }
@@ -144,77 +143,6 @@ public final class AppZygote implements ZygotePreload {
                             Log.e(TAG, "Close selinux netlink socket", e);
                         }
                     }
-                }
-            }
-        }
-    }
-
-    private static boolean contextExists(String context) {
-        var data = context.getBytes(StandardCharsets.UTF_8);
-
-        try (var file = new FileOutputStream("/sys/fs/selinux/context")) {
-            Os.write(file.getFD(), data, 0, data.length);
-            return true;
-        } catch (ErrnoException e) {
-            if (e.errno != OsConstants.EINVAL) {
-                throw new RuntimeException("security_check_context errno=" + e.errno, e);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("security_check_context: " + e.getMessage(), e);
-        }
-
-        if (SELinux.checkSELinuxAccess("u:r:app_zygote:s0", context, "process", "dyntransition")) {
-            return true;
-        }
-
-        try (var current = new FileOutputStream("/proc/self/attr/current")) {
-            Os.write(current.getFD(), data, 0, data.length);
-            throw new RuntimeException("SELinux is broken");
-        } catch (ErrnoException e) {
-            if (e.errno == OsConstants.EINVAL) {
-                return false;
-            }
-            if (e.errno == OsConstants.EPERM) {
-                return true;
-            }
-            throw new RuntimeException("setcon errno=" + e.errno, e);
-        } catch (IOException e) {
-            throw new RuntimeException("setcon: " + e.getMessage(), e);
-        }
-    }
-
-    private static ByteBuffer readStatus() {
-        try (var status = new FileInputStream("/sys/fs/selinux/status")) {
-            var buffer = ByteBuffer.allocate(20);
-            buffer.order(ByteOrder.nativeOrder());
-            Os.pread(status.getFD(), buffer, 0);
-            return buffer;
-        } catch (ErrnoException e) {
-            throw new RuntimeException("read_sequence errno=" + e.errno, e);
-        } catch (IOException e) {
-            throw new RuntimeException("read_sequence: " + e.getMessage(), e);
-        }
-    }
-
-    private static int readAvdSeqNo() {
-        FileDescriptor fd = null;
-        try {
-            fd = Os.open("/sys/fs/selinux/access", OsConstants.O_RDWR | OsConstants.O_CLOEXEC, 0);
-            var bytes = "u:r:untrusted_app:s0 u:r:untrusted_app:s0 0".getBytes();
-            Os.write(fd, ByteBuffer.wrap(bytes));
-            // %x %x %x %x %u %x = (2+8)*5 + 10 + 5 = 65 ~= 128
-            var result = ByteBuffer.allocate(128);
-            var ret = Os.read(fd, result);
-            var str = new String(result.array(), 0, ret);
-            return Integer.parseInt(str.split(" ")[4]);
-        } catch (Throwable t) {
-            Log.e(TAG, "readAvdSeqNo err", t);
-            return -1;
-        } finally {
-            if (fd != null) {
-                try {
-                    Os.close(fd);
-                } catch (ErrnoException ignored) {
                 }
             }
         }
---
+++ app/src/main/java/org/lsposed/dirtysepolicy/SELinux.java
@@ -0,0 +1,149 @@
+package org.lsposed.dirtysepolicy;
+
+import android.system.ErrnoException;
+import android.system.Os;
+import android.system.OsConstants;
+import android.util.Pair;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SELinux {
+    private static final Map<String, Pair<Integer, Integer>> permCache = new HashMap<>();
+
+    public static Pair<Integer, Integer> readIndex(String className, String permName) {
+        var classDir = Paths.get("/sys/fs/selinux/class");
+        var key = className + ":" + permName;
+        var value = permCache.get(key);
+        if (value != null) {
+            return value;
+        }
+        try {
+            var clazz = classDir.resolve(className);
+            var bytes = Files.readAllBytes(clazz.resolve("index"));
+            var classId = Integer.parseInt(new String(bytes));
+
+            var perm = clazz.resolve("perms").resolve(permName);
+            var permId = Integer.parseInt(new String(Files.readAllBytes(perm)));
+            value = Pair.create(classId, 1 << (permId - 1));
+            permCache.put(key, value);
+            return value;
+        } catch (IOException e) {
+            throw new RuntimeException("readId: " + e.getMessage(), e);
+        }
+    }
+
+    public static String[] access(String scon, String tcon, int tclass) throws ErrnoException {
+        try (var access = new RandomAccessFile("/sys/fs/selinux/access", "rw")) {
+            var query = scon + " " + tcon + " " + tclass;
+            var data = query.getBytes(StandardCharsets.UTF_8);
+            Os.write(access.getFD(), data, 0, data.length);
+            // %x %x %x %x %u %x = 8 * 5 + 10 + 5 = 55 ~= 64
+            var result = new byte[64];
+            var ret = access.read(result);
+            var str = new String(result, 0, ret);
+            var avd = str.split(" ");
+            if (avd.length != 6) {
+                throw new RuntimeException("Invalid access result: " + str);
+            }
+            return avd;
+        } catch (IOException e) {
+            throw new RuntimeException("access: " + e.getMessage(), e);
+        }
+    }
+
+    public static boolean checkSELinuxAccess(String scon, String tcon, String tclass, String perm) {
+        var pair = readIndex(tclass, perm);
+        try {
+            var parts = access(scon, tcon, pair.first);
+            var allowd = Integer.parseInt(parts[0], 16);
+            return (allowd & pair.second) == pair.second;
+        } catch (ErrnoException e) {
+            if (e.errno == OsConstants.EINVAL) {
+                return false;
+            }
+            throw new RuntimeException("checkSELinuxAccess: " + e.getMessage(), e);
+        }
+    }
+
+    public static boolean contextExists(String context) {
+        var data = context.getBytes(StandardCharsets.UTF_8);
+
+        try (var file = new FileOutputStream("/sys/fs/selinux/context")) {
+            Os.write(file.getFD(), data, 0, data.length);
+            return true;
+        } catch (ErrnoException e) {
+            if (e.errno != OsConstants.EINVAL) {
+                throw new RuntimeException("security_check_context errno=" + e.errno, e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("security_check_context: " + e.getMessage(), e);
+        }
+
+        try {
+            access("u:r:app_zygote:s0", context, 0);
+            return true;
+        } catch (ErrnoException e) {
+            if (e.errno != OsConstants.EINVAL) {
+                throw new RuntimeException("selinux_check_access: " + e.getMessage(), e);
+            }
+        }
+
+        try (var current = new FileOutputStream("/proc/self/attr/current")) {
+            Os.write(current.getFD(), data, 0, data.length);
+            throw new RuntimeException("SELinux is broken");
+        } catch (ErrnoException e) {
+            if (e.errno == OsConstants.EINVAL) {
+                return false;
+            }
+            if (e.errno == OsConstants.EPERM) {
+                return true;
+            }
+            throw new RuntimeException("setcon errno=" + e.errno, e);
+        } catch (IOException e) {
+            throw new RuntimeException("setcon: " + e.getMessage(), e);
+        }
+    }
+
+    public static ByteBuffer readStatus() {
+        try (var status = new FileInputStream("/sys/fs/selinux/status")) {
+            var buffer = ByteBuffer.allocate(20);
+            buffer.order(ByteOrder.nativeOrder());
+            Os.read(status.getFD(), buffer);
+            return buffer;
+        } catch (ErrnoException e) {
+            throw new RuntimeException("read_status errno=" + e.errno, e);
+        } catch (IOException e) {
+            throw new RuntimeException("read_status: " + e.getMessage(), e);
+        }
+    }
+
+    public static boolean isSELinuxEnabled() {
+        return android.os.SELinux.isSELinuxEnabled();
+    }
+
+    public static boolean isSELinuxEnforced() {
+        return android.os.SELinux.isSELinuxEnforced();
+    }
+
+    public static String getFileContext(String path) {
+        return android.os.SELinux.getFileContext(path);
+    }
+
+    public static String getContext() {
+        return android.os.SELinux.getContext();
+    }
+
+    public static String getPidContext(int pid) {
+        return android.os.SELinux.getPidContext(pid);
+    }
+}
